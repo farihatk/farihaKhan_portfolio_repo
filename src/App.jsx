23d71c5df9f220eea7f201292af5e3a9f16d6f4e
Fariha Khan <fkhan88@my.bcit.ca>
2026-06-02 20:12:28 -0700

    update app.jsx with route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import PageWrapper from "./components/PageWrapper"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import Playground from "./pages/Playground"
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PageWrapper />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="playground" element={<Playground />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
