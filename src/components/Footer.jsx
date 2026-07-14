44ac15e5d7b33f691f846bf887be8f446b0a0a0f
Fariha Khan <fkhan88@my.bcit.ca>
2026-07-13 21:08:26 -0700

    added icon btns to home
    Also restyled footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,38 +1,61 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEnvelope, faFileArrowDown } from "@fortawesome/free-solid-svg-icons"
+import { faInstagram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
 
 import "./Footer.css"
 
 function Footer() {
+  const [copied, setCopied] = useState(false);
+
+  function copyEmail() {
+    navigator.clipboard.writeText("fariha.khan2805@outlook.com")
+    setCopied(true)
+    setTimeout(() => setCopied(false), 2000)
+  }
+
   return (
     <footer className="footer">
-      <div className="footer__inner">
-
-        <div className="footer__left">
-          <h2 className="footer__name">Fariha Khan</h2>
-          <div className="footer__cta">
-            <a href="mailto:fariha.t.khan99@gmail.com" className="buttons footer__hi-btn">
+      <div className="container">
+        <div className="grid">
+          <div className="footer__left col-6">
+            <p className="footer__name">Fariha Khan</p>
+            <button className="footer__hi-btn" onClick={copyEmail}>
               Say hi to me!
-            </a>
-            <a href="mailto:fariha.t.khan99@gmail.com" className="footer__mail-icon icon-buttons" aria-label="Send email">
-              <img src="/assets/mail-icon.svg" alt="Mail" />
-            </a>
+              <span className="footer__hi-btn-icon">
+                <FontAwesomeIcon icon={faEnvelope} />
+              </span>
+              {copied && <span className="footer__email-tooltip">Email copied!</span>}
+            </button>
+
           </div>
+          <nav className="footer__nav col-6">
+            <div className="footer__nav-col">
+              <Link to="/">Home</Link>
+              <Link to="/projects">Work</Link>
+              <Link to="/about">About</Link>
+              <Link to="/playground">Playground</Link>
+            </div>
+            <div className="footer__nav-col">
+              <a href="https://instagram.com/yourhandle" target="_blank" rel="noreferrer">
+                <span className="footer__social-icon">
+                  <FontAwesomeIcon icon={faInstagram} />
+                </span>
+              </a>
+              <a href="https://instagram.com/yourhandle" target="_blank" rel="noreferrer">
+                <span className="footer__social-icon">
+                  <FontAwesomeIcon icon={faLinkedinIn} />
+                </span>
+              </a>
+              <a href="#" download>
+                <span className="footer__social-icon">
+                  <FontAwesomeIcon icon={faFileArrowDown} />
+                </span>
+              </a>
+            </div>
+          </nav>
         </div>
-
-        <nav className="footer__nav">
-          <div className="footer__nav-col">
-            <Link to="/">Home</Link>
-            <Link to="/projects">Work</Link>
-            <Link to="/about">About</Link>
-            <Link to="/playground">Playground</Link>
-          </div>
-          <div className="footer__nav-col">
-            <a href="/assets/resume.pdf" target="_blank" rel="noreferrer">Resume</a>
-            <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noreferrer">LinkedIn</a>
-            <a href="https://instagram.com/yourhandle" target="_blank" rel="noreferrer">Instagram</a>
-          </div>
-        </nav>
-
       </div>
 
       <p className="footer__credit">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,39 @@
+import { useState } from 'react'
 import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 
 import "./Header.css"
 
 import FkLogo from "../assets/fk-logo.png"
 
 function Header() {
+  const [copied, setCopied] = useState(false);
+
+  function copyEmail() {
+    navigator.clipboard.writeText("fariha.khan2805@outlook.com")
+    setCopied(true)
+    setTimeout(() => setCopied(false), 2000)
+  }
+
   return (
     <header className="header">
       <div className="header__container">
 
-        <div className="header__brand">
+        <Link to="/" className="header__brand">
           <img src={FkLogo} alt="Brand logo" className="header__logo" />
           <span className="header__name">Fariha Khan</span>
-        </div>
+        </Link>
 
         <nav className="header__nav">
           <Link to="/projects" className="nav-buttons">Work</Link>
           <Link to="/about" className="nav-buttons">Story</Link>
           <Link to="/playground" className="nav-buttons">Playground</Link>
+          <button className="header__email-btn" onClick={copyEmail} aria-label="Copy email address">
+            <FontAwesomeIcon icon={faEnvelope} />
+            {copied && <span className="header__email-tooltip">Email copied!</span>}
+          </button>
         </nav>
-
-        {/* <a href="mailto:fariha.khan2805@outlook.com" className="header__mail icon-buttons" aria-label="Send email">
-          <img src="/assets/mail-icon.svg" alt="Mail" />
-        </a> */}
 
       </div>
     </header>
