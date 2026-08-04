9c4d959d991fc42906102c1df845b388202066c3
Fariha Khan <fkhan88@my.bcit.ca>
2026-08-04 11:24:23 -0700

    convert motion project preview from mov to mp4

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,6 @@
 import HeyloFeatured from "../assets/heylo-featured.jpg"
 import PalaceFeatured from "../assets/palace-mockup3.jpg"
-import BTSPreviewVideo from "../assets/bts-motion-preview.mov"
+import BTSPreviewVideo from "../assets/bts-motion-preview.mp4"
 import BTSPreviewImg from "../assets/bts-motion-previewImg.jpg"
 import MakioFeatured from "../assets/makio-featured2.png"
 
